4ec60cef60431fe73a700c0101caf40429e71445
Rasmus Jõenurm <you@example.com>
2025-10-30 12:49:08 +0200

    add placeholders for pages (for now)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h1>Welcome to Charry</h1>
-            <p>Brrr</p>
+            <p>placeholder</p>
         </div>
     )
 }
--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -2,7 +2,7 @@ function Partners() {
     return (
         <div>
             <h1>Our Partners</h1>
-            <p>Juhuu</p>
+            <p>placeholder</p>
         </div>
     )
 }
