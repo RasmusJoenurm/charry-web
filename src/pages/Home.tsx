01d1de169ad64fa44334ebc305f299fb4e5b5a11
RasmusJoenurm <rjoenurm@gmail.com>
2025-11-04 13:36:26 +0200

    make bottomContainer on Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
+import styles from "./Home.module.css"
+
 function Home() {
     return (
-        <div>
-            <h1>Welcome to Charry</h1>
-            <p>placeholder</p>
-        </div>
+        <>
+            <div style={{ height: '300vh', padding: '2rem' }}>
+                <h1 style={{ color: 'white' }}>Welcome to Charry</h1>
+                <p style={{ color: 'white' }}>This pushes content down...</p>
+            </div>
+            <div className={styles.bottomContainer}>
+                <div className={styles.textContainer}>
+                    <p className={styles.line1}>100+ annetajat</p>
+                    <p className={styles.line2}>Koos loome suure mõju!</p>
+                </div>
+            </div>
+        </>
     )
 }
 
