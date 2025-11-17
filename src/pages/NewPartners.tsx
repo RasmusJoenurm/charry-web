d3635273887b41228977ba53ea728ebc10ba7aba
RasmusJoenurm <rjoenurm@gmail.com>
2025-11-17 19:30:54 +0200

    add a join us section to NewPartners page

--- src/pages/NewPartners.tsx
+++ src/pages/NewPartners.tsx
@@ -1,9 +1,16 @@
+import styles from "./NewPartners.module.css"
+
 function NewPartners() {
     return (
-        <div>
-            <h1>New partners</h1>
-            <p>placeholder</p>
-        </div>
+        <>
+            <div className={styles.joinContainer}>
+                <h2 className={styles.joinHeading}>Ole üks meie MTÜ-dest</h2>
+                <div className={styles.textButtonContainer}>
+                    <p className={styles.joinText}>Järgnevate sammude arutamiseks võta meiega ühendust.</p>
+                    <a href="mailto:robert@charry.ee?subject?Contact%20Request" className={styles.contactButton}>Võta ühendust</a>
+                </div>
+            </div>
+        </>
     )
 }
 
