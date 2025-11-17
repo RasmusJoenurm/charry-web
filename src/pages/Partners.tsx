20000ca110387fbd5f560083ae8257c2e58ad4c5
RasmusJoenurm <rjoenurm@gmail.com>
2025-11-17 18:34:22 +0200

    make our friends section on Partners page

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+
+import styles from "./Partners.module.css"
+
 function Partners() {
     return (
-        <div>
-            <h1>Our Partners</h1>
-            <p>placeholder</p>
-        </div>
+        <>
+            <div className={styles.friendsContainer}>
+                <h2 className={styles.friendsHeading}>Meie sõbrad</h2>
+                <div className={styles.textButtonContainer}>
+                    <p className={styles.friendsText}>Kui soovid, et sinu usaldusväärne ja hästi toimiv heategevusorganisatsioon oleks Charry platvormil, uuri rohkem.</p>
+                    <Link to="/NewPartners" className={styles.newPartnersButton}>Uued MTÜ-d</Link>
+                </div>
+            </div>
+        </>
     )
 }
 
