93f6065a25c722de05778908875581d3209dd884
RasmusJoenurm <rjoenurm@gmail.com>
2025-11-25 19:12:51 +0200

    finish newPartners page translations

--- src/pages/NewPartners.tsx
+++ src/pages/NewPartners.tsx
@@ -1,18 +1,23 @@
+import { useTranslation } from "react-i18next"
+
 import styles from "./NewPartners.module.css"
 import bulletIcon from "../assets/heartBulletPoint.svg"
 import rightSideImage from "../assets/newPartnersMobile.svg"
 import divider from "../assets/heartDivider.svg"
 
 function NewPartners() {
+
+    const { t } = useTranslation();
+
     return (
         <>
             <div className={styles.headingContainer}>
-                <h1 className={styles.headingTop}>Ettevõtete väljakutsed</h1>
+                <h1 className={styles.headingTop}>{t("newPartners.main.title")}</h1>
             </div>
             <div className={styles.challengesMainContent}>
                 <div className={styles.textMainContent}>
                     <div className={styles.introSection}>
-                        <p className={styles.introText}>Annetajate usaldus on meie peamine prioriteet. Seetõttu veendume enne platvormile lisamist, et heategevusorganisatsioon on usaldusväärne ja tõhusalt toimiv. <br /> <br /> Meie jaoks on olulised nii korras olevad dokumendid kui ka säravad silmad ja siiras soov ühiskonda aidata.</p>
+                        <p className={styles.introText}>{t("newPartners.main.titleText1")}<br /> <br /> {t("newPartners.main.titleText2")}</p>
                     </div>
                     <div className={styles.heartDivider}>
                         <img src={divider} className={styles.heartDividerImage}></img>
@@ -21,32 +26,32 @@ function NewPartners() {
                     <div className={styles.bulletPointContainer}>
                         <div className={styles.bulletPoint}>
                             <img src={bulletIcon} className={styles.bulletIcon} alt="" />
-                            <p className={styles.bulletText}>Charry meeskond tagab, et kõik platvormiga seotud protsessid oleksid heategevusorganisatsiooni jaoks võimalikult mugavad ja sujuvalt toimivad.</p>
+                            <p className={styles.bulletText}>{t("newPartners.main.bulletpoint1")}</p>
                         </div>
                         <div className={styles.bulletPoint}>
                             <img src={bulletIcon} className={styles.bulletIcon} alt="" />
-                            <p className={styles.bulletText}>Kõik platvormil tehtud annetused suunatakse koheselt otse heategevusorganisatsioonidele.</p>
+                            <p className={styles.bulletText}>{t("newPartners.main.bulletpoint2")}</p>
                         </div>
                         <div className={styles.bulletPoint}>
                             <img src={bulletIcon} className={styles.bulletIcon} alt="" />
                             <div className={styles.textWrapper}>
-                                <p className={styles.bulletText}>Charry platvormil saavad heategevusorganisatsioonid mugavalt jagada oma annetajatele (visuaalset) tagasisidet. </p>
-                                <p className={styles.bulletTextSmall}>* Charry meeskond on alati valmis aitama tagasiside sisu loomisel, näiteks fotode ja videote tegemisel.</p>
+                                <p className={styles.bulletText}>{t("newPartners.main.bulletpoint3")}</p>
+                                <p className={styles.bulletTextSmall}>{t("newPartners.main.smallText")}</p>
                             </div>
                         </div>
                         <div className={styles.bulletPoint}>
                             <img src={bulletIcon} className={styles.bulletIcon} alt="" />
-                            <p className={styles.bulletText}>Platvormiga liitumiseks peab heategevusorganisatsioon olema kantud tulumaksusoodustusega ühingute nimekirja.</p>
+                            <p className={styles.bulletText}>{t("newPartners.main.bulletpoint4")}</p>
                         </div>
                     </div>
                 </div>
                 <img src={rightSideImage} className={styles.sideImage} alt="" />
             </div>
             <div className={styles.joinContainer}>
-                <h2 className={styles.joinHeading}>Ole üks meie MTÜ-dest</h2>
+                <h2 className={styles.joinHeading}>{t("newPartners.join.title")}</h2>
                 <div className={styles.textButtonContainer}>
-                    <p className={styles.joinText}>Järgnevate sammude arutamiseks võta meiega ühendust.</p>
-                    <a href="mailto:robert@charry.ee?subject?Contact%20Request" className={styles.contactButton}>Võta ühendust</a>
+                    <p className={styles.joinText}>{t("newPartners.join.titleText")}</p>
+                    <a href="mailto:robert@charry.ee?subject?Contact%20Request" className={styles.contactButton}>{t("newPartners.join.buttonText")}</a>
                 </div>
             </div>
         </>
