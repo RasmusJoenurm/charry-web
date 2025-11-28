ccfe23aad7a94cbfd5584a6aa6f30df16c717fd7
RasmusJoenurm <rjoenurm@gmail.com>
2025-11-28 16:46:45 +0200

    finish mobil design for Home page yellow container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,11 @@ import huggingBird from "../assets/birdHugging.svg"
 import wavingBird from "../assets/birdWaving.svg"
 import mobilePhones from "../assets/mobilePhones.svg"
 
+import appStoreEst from "../assets/appStoreEst.svg"
+import appStoreEng from "../assets/appStoreEng.svg"
+import googlePlayEst from "../assets/googlePlayEst.svg"
+import googlePlayEng from "../assets/googlePlayEng.svg"
+
 import QRModal from "../components/QRModal"
 
 function Home() {
@@ -21,7 +26,7 @@ function Home() {
 
     const [userCount, setUserCount] = useState<number>(100);
 
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
 
     useEffect(() => {
         fetch("https://backend-impact-203432700423.europe-west1.run.app/user/currentNumberOfUsers")
@@ -36,7 +41,14 @@ function Home() {
                 <div className={styles.textWithBirds}>
                     <h1 className={styles.yellowHeading}>{t("home.yellow.titleLine1")}<br /> {t("home.yellow.titleLine2")}</h1>
                     <p className={styles.yellowSubHeading}>{t("home.yellow.subLine1")}<br /> {t("home.yellow.subLine2")}<br /> {t("home.yellow.subLine3")}</p>
-
+                    <div className={styles.storeButtons}>
+                        <a href="https://apps.apple.com/ee/app/charry-annetus%C3%A4pp/id6748387002" className={styles.storeButton}>
+                            <img src={i18n.language === "et" ? appStoreEst : appStoreEng} alt="Laadi alla App Store’is" />
+                        </a>
+                        <a href="market://details?id=com.charryapp.android" className={styles.storeButton}>
+                            <img src={i18n.language === "et" ? googlePlayEst : googlePlayEng} alt="Laadi alla Google Play’s" />
+                        </a>
+                    </div>
                     <img src={huggingBird} alt="hugging bird" className={styles.huggingBird} />
                     <img src={wavingBird} alt="waving bird" className={styles.wavingBird} />
                 </div>
